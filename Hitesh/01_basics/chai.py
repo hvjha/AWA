9ff31a73896d465ecb5b121e943400130efbcb50
Harsh Vardhan Jha <harsh.jha@attentive.ai>
2026-02-09 11:09:11 +0530

    "Hitesh1_lect"

---
+++ Hitesh/01_basics/chai.py
@@ -0,0 +1,8 @@
+from helloChai import add, subtract, multiply, divide
+# Example usage
+num1 = 20
+num2 = 4    
+print("Addition:", add(num1, num2))
+print("Subtraction:", subtract(num1, num2))     
+print("Multiplication:", multiply(num1, num2))
+print("Division:", divide(num1, num2))
---
+++ Hitesh/01_basics/helloChai.py
@@ -0,0 +1,22 @@
+# print("Hello Chain  of Thought!")
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b        
+def divide(a, b):
+    if b != 0:
+        return a // b
+    else:
+        return "Cannot divide by zero"  
+    
+# Example usage
+num1 = 10   
+num2 = 5    
+print("Addition:", add(num1, num2))
+print("Subtraction:", subtract(num1, num2)) 
+print("Multiplication:", multiply(num1, num2))
+print("Division:", divide(num1, num2))
